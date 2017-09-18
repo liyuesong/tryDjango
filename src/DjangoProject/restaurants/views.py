28951a1cef09ecccedc9f8293ab2b54d1fa79cc6
liyuesong <liyuesong1943@gmail.com>
2017-09-18 00:05:37 -0700

    template

---
+++ src/DjangoProject/restaurants/views.py
@@ -0,0 +1,44 @@
+import random
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# handle URLs
+# function based view
+# def home_old(request):
+# 	# put f string in the html: string substitution
+# 	html_var = 'f strings'
+# 	html_ = f"""
+# 	<!DOCTYPE html>
+# 	<html lang="en">
+#   	<head>
+#   	</head>
+#   	<body>
+#   	<h1>Hello World!</h1>
+#   	<p>This is {html_var} coming through</p>
+#   	</body>
+#   	</html>
+# 	""" # use triple " because it is a long string
+# 	# f strings in Python 3: to do string substitution
+# 	return HttpResponse(html_)
+# 	#return render(request, "home.html", {}) #three args: req, template(jump to), context of the template #response
+
+def home(request):
+	num = None
+	# some_list = [num, random.randint(0, 10000000), random.randint(0, 10000000)]
+	some_list = [
+		random.randint(0, 10000000), 
+		random.randint(0, 10000000), 
+		random.randint(0, 10000000)
+	]
+	# logic in the view itself, not so much in the template
+	condition_bool_item = True
+	if condition_bool_item:
+		num = random.randint(0, 10000000)
+	context = {
+		# "bool_item": True, 
+		"num": num, 
+		"some_list": some_list
+	}
+	# return render(request, "base.html", {"html_var": "context variable"})
+	return render(request, "base.html", context) # replace the context variable in the html/template
