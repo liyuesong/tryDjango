8b289aa91f6d6f2c0b2e22796c1a186efda328dc
liyuesong <liyuesong1943@gmail.com>
2017-12-10 16:13:01 -0800

    template view

--- src/DjangoProject/restaurants/views.py
+++ src/DjangoProject/restaurants/views.py
@@ -2,6 +2,7 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 # handle URLs
@@ -24,49 +25,79 @@ from django.views import View
 # 	return HttpResponse(html_)
 # 	#return render(request, "home.html", {}) #three args: req, template(jump to), context of the template #response
 
-def home(request):
-	num = None
-	# some_list = [num, random.randint(0, 10000000), random.randint(0, 10000000)]
-	some_list = [
-		random.randint(0, 10000000), 
-		random.randint(0, 10000000), 
-		random.randint(0, 10000000)
-	]
-	# logic in the view itself, not so much in the template
-	condition_bool_item = True
-	if condition_bool_item:
-		num = random.randint(0, 10000000)
-	context = {
-		# "bool_item": True, 
-		"num": num, 
-		"some_list": some_list
-	}
-	# return render(request, "base.html", {"html_var": "context variable"})
-	return render(request, "home.html", context) # replace the context variable in the html/template
+####### function base views ##########
+# def home(request):
+# 	num = None
+# 	# some_list = [num, random.randint(0, 10000000), random.randint(0, 10000000)]
+# 	some_list = [
+# 		random.randint(0, 10000000), 
+# 		random.randint(0, 10000000), 
+# 		random.randint(0, 10000000)
+# 	]
+# 	# logic in the view itself, not so much in the template
+# 	condition_bool_item = True
+# 	if condition_bool_item:
+# 		num = random.randint(0, 10000000)
+# 	context = {
+# 		# "bool_item": True, 
+# 		"num": num, 
+# 		"some_list": some_list
+# 	}
+# 	# return render(request, "base.html", {"html_var": "context variable"})
+# 	return render(request, "home.html", context) # replace the context variable in the html/template
 
-def about(request):
-	context = {
-	}
-	# return render(request, "base.html", {"html_var": "context variable"})
-	return render(request, "about.html", context) # replace the context variable in the html/template
+# def about(request):
+# 	context = {
+# 	}
+# 	# return render(request, "base.html", {"html_var": "context variable"})
+# 	return render(request, "about.html", context) # replace the context variable in the html/template
 
-def contact(request):
-	context = {
-	}
-	# return render(request, "base.html", {"html_var": "context variable"})
-	return render(request, "contact.html", context) # replace the context variable in the html/template
+# def contact(request):
+# 	context = {
+# 	}
+# 	# return render(request, "base.html", {"html_var": "context variable"})
+# 	return render(request, "contact.html", context) # replace the context variable in the html/template
 
-class ContactView(View):
-	def get(self, request, *args, **kwargs):
-		context = {}
-		return render(request, "contact.html", context)
 
-	# def post(self, request, *args, **kwargs):
-	# 	return
+####### class base views #########
+# class ContactView(View):
+# 	def get(self, request, *args, **kwargs):
+# 		context = {}
+# 		return render(request, "contact.html", context)
 
-	# def put(self, request, *args, **kwargs):
-	# 	return
+# 	# def post(self, request, *args, **kwargs):
+# 	# 	return
 
+# 	# def put(self, request, *args, **kwargs):
+# 	# 	return
+
+######## template views ############
+class HomeView(TemplateView):
+	template_name = 'home.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		# print(context)
+		num = None
+		some_list = [
+			random.randint(0, 10000000), 
+			random.randint(0, 10000000), 
+			random.randint(0, 10000000)
+		]
+		condition_bool_item = True
+		if condition_bool_item:
+			num = random.randint(0, 10000000)
+		context = {
+			"num": num, 
+			"some_list": some_list
+		}
+		return context
+
+# class AboutView(TemplateView):
+# 	template_name = 'about.html'
+
+# class ContactTemplateView(TemplateView):
+# 	template_name = 'contact.html'
 
 
 
